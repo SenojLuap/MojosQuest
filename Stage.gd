f348a2094155e21bc4e61f08522240337f4deb3e
Paul Jones <shirlypunk@hotmail.com>
2019-04-10 19:36:12 -0400

    tuna cans are now collectable

--- Stage.gd
+++ Stage.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 class_name Stage
 
-var TunaSpawnLocs = []
-var TunaCans = 0
+var TunaSpawnLocs := []
+var TunaCans : int = 0
 
 export var MaxTuna = 2
 
@@ -12,7 +12,7 @@ func _ready():
 
 func player_get_tuna(tuna : TunaCan):
 	TunaSpawnLocs.append(tuna.position)
-	remove_child(tuna)
+	tuna.queue_free()
 	TunaCans -= 1
 
 
@@ -20,8 +20,8 @@ func update_tunas():
 	if TunaCans < MaxTuna and TunaSpawnLocs.size() > 0:
 		var newLocIdx = randi() % TunaSpawnLocs.size()
 		#var newCan = TunaCan.new()
-		var canScene = load("res://TunaCan.tscn")
-		var newCan = canScene.instance()
+		var canScene := load("res://TunaCan.tscn")
+		var newCan : TunaCan = canScene.instance() as TunaCan
 		add_child(newCan)
 		newCan.set_owner(self)
 		newCan.position = TunaSpawnLocs[newLocIdx]
