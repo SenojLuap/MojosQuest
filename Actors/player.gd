715b37753246ab413a5b87560fac6888c23f508f
Paul Jones <shirlypunk@hotmail.com>
2019-04-15 19:18:09 -0400

    tweaked player physics

--- Actors/player.gd
+++ Actors/player.gd
@@ -1,23 +1,22 @@
 extends KinematicBody2D
 
-var momentum = Vector2(0.0, 0.0)
-
-
+var momentum := Vector2.ZERO;
 
 func _physics_process(delta) :
-	if (is_on_floor()):
-		momentum.y = 0.0
-	else:
-		momentum += (PlayerGlobals.GRAVITY * delta)
-	if Input.is_action_just_pressed("ui_jump") and is_on_floor():
-		momentum += PlayerGlobals.JUMP_IMPULSE
-		
-	if Input.is_action_pressed("ui_right"):
-		momentum.x = PlayerGlobals.WALK_SPEED
-	elif Input.is_action_pressed("ui_left"):
-		momentum.x = -PlayerGlobals.WALK_SPEED
-	else:
-		momentum.x = 0
+	momentum.x = 0;
+	var snap_vec := Vector2.DOWN * 8;
 	
-	move_and_slide(momentum, Globals.FLOOR_NORMAL)
+	if (!is_on_floor()):
+		momentum += PlayerGlobals.GRAVITY;
+		snap_vec = Vector2.ZERO;
+	else:
+		momentum.y = 0;
+		if (Input.is_action_just_pressed("ui_jump")):
+			momentum += PlayerGlobals.JUMP_IMPULSE;
+			
+	if (Input.is_action_pressed("ui_left")):
+		momentum.x -= PlayerGlobals.WALK_SPEED;
+	if (Input.is_action_pressed("ui_right")):
+		momentum.x += PlayerGlobals.WALK_SPEED;
+	move_and_slide_with_snap(momentum, snap_vec, Vector2.UP);
 	
--- PlayerGlobals.gd
+++ PlayerGlobals.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const GRAVITY = Vector2(0.0, 500.0) # Force of gravity in pixels/sec
-const JUMP_IMPULSE = Vector2(0.0, -300)
+const GRAVITY = Vector2(0.0, 50.0) # Force of gravity in pixels/sec
+const JUMP_IMPULSE = Vector2(0.0, -650)
 
-const WALK_SPEED = 150
+const WALK_SPEED = 275
