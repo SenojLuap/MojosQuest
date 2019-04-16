007803dbb0a64adf7ac36c735b2b744787c561aa
Paul Jones <shirlypunk@hotmail.com>
2019-04-16 18:08:54 -0400

    added 'pounce' ability'

--- PlayerGlobals.gd
+++ PlayerGlobals.gd
@@ -3,4 +3,8 @@ extends Node
 const GRAVITY = Vector2(0.0, 50.0) # Force of gravity in pixels/sec
 const JUMP_IMPULSE = Vector2(0.0, -650)
 
+const MAX_POUNCE_TIME : float = 1.5; # Max time the player may build-up a 'pounce'
+const MIN_POUNCE : float = 1.0; # Minimum 'pounce' impulse modifier
+const MAX_POUNCE : float = 1.5; # Maximum 'pounce' impulse modifier
+
 const WALK_SPEED = 275
